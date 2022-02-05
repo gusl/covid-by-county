6bfeb49543e08d1b79186b7b9a70d3e1c71faa4f
gusl <gusg@optimizelife.com>
2022-02-04 17:55:36 -0800

    updated

--- covid.R
+++ covid.R
@@ -23,11 +23,11 @@ dt[, cfr := deaths / cases]
 dt[, state.county := paste0(state, ":", county)]
 dt[, id := paste0(state, ":", county, ":", fips)]
 
-counies <- data.frame(state=c("CA", "CA", "CA", "CA", "CA", "CA", "CA", "CA", "CA"),
-                      county=c("San Francisco", "San Mateo", "Santa Clara", "Alameda", "Contra Costa", "Marin", "Sonoma", "Solano", "Napa"))#, "Santa Cruz"))
-counies$population <- c(870044, 765935, 1922200, 1643700, 1133247, 260295, 501317, 438530, 137744)#, 273213)
-counies$city.name <- paste(counies$county)
-counies <- data.frame(state=c("FL", "FL", "WA", ##"FL",
+# counties <- data.frame(state=c("CA", "CA", "CA", "CA", "CA", "CA", "CA", "CA", "CA"),
+#                       county=c("San Francisco", "San Mateo", "Santa Clara", "Alameda", "Contra Costa", "Marin", "Sonoma", "Solano", "Napa"))#, "Santa Cruz"))
+# counties$population <- c(870044, 765935, 1922200, 1643700, 1133247, 260295, 501317, 438530, 137744)#, 273213)
+# counties$city.name <- paste(counties$county)
+counties <- data.frame(state=c("FL", "FL", "WA", ##"FL",
                               "NY", "MA", "PA",
                               "CA", "AZ", "GA",
                               "CA", "IL", "TX"),
@@ -35,25 +35,22 @@ counies <- data.frame(state=c("FL", "FL", "WA", ##"FL",
                                "New York City", "Suffolk", "Allegheny",
                                "San Francisco", "Maricopa", "Fulton",
                                "Los Angeles", "Cook", "Harris"))
-counies$population <- c(2717000, 1953000, 2253000, ##1497000,
+counties$population <- c(2717000, 1953000, 2253000, ##1497000,
                         8419000, 803907, 1216000,
                         874691, 4485000, 1064000,
                         10040000, 5150000, 4713000)#, 273213)
-counies$city.name <- c("Miami", "Miami", "Seattle", ## Miami
+counties$city.name <- c("Miami", "Miami", "Seattle", ## Miami
                        "", "Boston", "Pittsburgh",
                        "", "Phoenix", "Atlanta",
                        "", "Chicago", "Houston")
 
 dt[, state.county := paste0(state, ":", county)]
-counies$state.county <- with(counies, paste0(state, ":", county))
-this.dt <- dt ##[ state.county %in% state.counties,]
+counties$state.county <- with(counties, paste0(state, ":", county))
+this.dt <- dt
 this.dt[, state.county := paste0(state, ":", county)]
-# this.dt <- dt[ date >= "2020-09-01", ]
 names(this.dt)
-names(counies)
-this.dt <- merge(this.dt, counies, by="state.county")
-## Fill in zeros
-##this.dt <- merge(, , all.x=TRUE)
+names(counties)
+this.dt <- merge(this.dt, counties, by="state.county")
 this.dt[, new.cases := c(filter(cases, c(1,-1))), by=state.county]
 this.dt[, new.cases.smo := filter(new.cases, rep(1/7, 7)), by=state.county]
 this.dt[, new.cases.smo := ifelse(is.na(new.cases.smo), 0, new.cases.smo)]
@@ -61,16 +58,16 @@ this.dt[, new.cases.smo.per.million := new.cases.smo * (1e6/population)]
 ymax <- max(this.dt[, this.dt$new.cases.smo.per.million])
 
 ## -----
-setDT(counies)                                        #counies
-counies <- counies[order(state.county)]
+setDT(counties)
+counties <- counties[order(state.county)]
 par(mfrow = c(4, 3), mai=rep(0.4, 4), las=1, mgp=c(3,0.5,0))
-for (i in seq_len(nrow(counies))) {
-    state. <- counies[i,]$state
-    county. <- counies[i,]$county
-    city.name. <- counies[i,]$city.name
+for (i in seq_len(nrow(counties))) {
+    state. <- counties[i,]$state
+    county. <- counties[i,]$county
+    city.name. <- counties[i,]$city.name
     city.name.shown <- ifelse(nchar(city.name.)>0,
                               paste0(" (", city.name., ")"), "")
-    pop. <- counies[i,]$population
+    pop. <- counties[i,]$population
     cat("county =", toString(county.), "\n")
     item <- dt[state==state.,][county==county., ][date >= "2020-03-15",]
     ##item <- dt[state==state. & county==county., ]
@@ -89,29 +86,27 @@ for (i in seq_len(nrow(counies))) {
     Axis(item$date, at = ticks.at, labels = labels, side = 1, las = 1, cex.axis = 0.6)
     abline(h=1000*(0:20), col="#00000050")
     abline(h=5000*(0:20), col="#000000A0")
-##    abline(h=100*(0:20), col="#00000020")
     abline(h=0, col="#00000080")
     reveillons <- sapply(paste0(2019:2022, "-01-01"), as.Date)
     abline(v=reveillons, col="#66666688", lty=1)
     with(item, points(date, 100*new.deaths.smo * (1e6 / pop.), type='l', col="red", lty=1))
     title(paste0(county., ", ", state., city.name.shown,
                 "\npop. ", round(pop., digits=3)))
-    #legend(as.Date("2020-03-01"), max(new.cases.smo, na.rm=TRUE), col=1:2, legend=c("new cases", "new deaths (x 20)"), lty=1, cex=0.7)
 }
 
 
 ## DELAGGED by 21 days
 filename <- paste0("~/covid-by-county_", today, "_delag21.png")
 res <- 100
-png(filename, res=res, width=1200, height=180*ceiling(nrow(counies) / 3))
-par(mfrow = c(nrow(counies) / 3, 3), mai=rep(0.5, 4), las=1, mgp=c(3,0.5,0))
-for (i in seq_len(nrow(counies))) {
-    state. <- counies[i,]$state
-    county. <- counies[i,]$county
-    city.name. <- counies[i,]$city.name
+png(filename, res=res, width=1200, height=180*ceiling(nrow(counties) / 3))
+par(mfrow = c(nrow(counties) / 3, 3), mai=rep(0.5, 4), las=1, mgp=c(3,0.5,0))
+for (i in seq_len(nrow(counties))) {
+    state. <- counties[i,]$state
+    county. <- counties[i,]$county
+    city.name. <- counties[i,]$city.name
     city.name.shown <- ifelse(nchar(city.name.)>0,
                               paste0(" (", city.name., ")"), "")
-    pop. <- counies[i,]$population
+    pop. <- counties[i,]$population
     cat("county =", toString(county.), "\n")
     item <- dt[state==state.,][county==county., ][date >= "2020-03-15",]
     ##item <- dt[state==state. & county==county., ]
